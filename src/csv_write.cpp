8f1070f757c04f043eb2662562dc5485fbfd3b82
trausi <trausi@fhwn.ac.at>
2023-06-11 22:13:30 +0200

    Adjusted csv_write so it would also take struct Room as a parameter

--- src/csv_write.cpp
+++ src/csv_write.cpp
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "student.h"
+#include "file.h"
 
-void write_csv(struct student *student, int num_students, const char *filename) {
+void write_csv(const char* filename, room* examRoom, student* student, int num_students) {
     FILE *fp = fopen("test.csv", "w+");
     if (fp == NULL) {
         printf("Error: Could not open file %s for writing\n", filename);
         return;
     }
+    // Write room header row
+    fprintf(fp, "Room name;Exame name; Exam date;row;col;occupancy\n");
+    fprintf(fp,"%s, %s, %s, %d, %d, %d\n",examRoom->room_name, examRoom->exam_name, examRoom->exam_date, examRoom->row, examRoom->col, examRoom->occupancy);
 
-    // Write header row
-    fprintf(fp, "ID;Name;Seat\n");
+    // Write student header row
+    fprintf(fp, "ID;First Name;Last Name;Seat\n");
 
     // Write data rows
     for (int i = 0; i < num_students; i++) {
-        fprintf(fp, "%s,%s,%s,%d,%d,\n",student[i].first_name, student[i].last_name,student[i].student_id, student[i].col,student[i].row);
+        fprintf(fp, "%s,%s,%s,%d,%d,\n", student[i].student_id, student[i].first_name, student[i].last_name, student[i].row, student[i].col);
     }
 
     fclose(fp);
