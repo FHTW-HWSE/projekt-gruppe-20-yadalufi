960e986fba8d6d47ed09433bd18a04ef579b9659
luciabeck <123989892+luciabeck@users.noreply.github.com>
2023-06-12 15:50:57 +0200

    added doxygen comments to quit.cpp ( and minor edits in other comments)

--- src/menu/quit.cpp
+++ src/menu/quit.cpp
@@ -4,6 +4,18 @@
 #include "../file.h"
 #include "../student.h"
 
+/**
+ * @brief       Quits the program and frees allocated memory.
+ *
+ * This function is called when the user chooses to quit the program. It displays a goodbye message,
+ * frees the memory allocated for the student and room structures, and performs necessary error handling.
+ *
+ * @param       m_room    Pointer to the pointer of the room structure.
+ * @param       m_student Pointer to the pointer of the student structure.
+ *
+ * @return      void
+ */
+
 void menu_quit(room **m_room, student **m_student)
 {
     printf("Goodbye!\n");
